5e116da6c4adb655fea6aede2833187473b5ec25
Tim Kleier <tim.kleier@elic.org>
2017-01-07 09:26:38 +0000

    figaro install and godaddy env variables

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# GoDaddy SMTP Settings
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+:address => ‘smtpout.secureserver.net’,
+:domain  => ‘www.theupgrade.blog’,
+:port      => 80,
+:user_name => ENV["godaddy_username"],
+:password => ENV["godaddy_password"],
+:authentication => :plain
+}
